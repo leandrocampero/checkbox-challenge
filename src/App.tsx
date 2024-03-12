6543e18ba630d58e377a0c1cac13dd84409c6cf9
Leandro Campero <leandrocampero14@gmail.com>
2024-03-12 00:27:40 -0300

    draft: basic behavior, not complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useMemo, useState } from 'react'
 import countries from './assets/countries.json'
 import './App.css'
 
-function handleClick(e: ChangeEvent<HTMLInputElement>) {
-  console.log(e.target.checked)
-}
+const SELECT_ALL_KEY = 'all'
 
 function App() {
+  const [isAllSelected, selectAll] = useState(false)
+
+  function handleClick(e: ChangeEvent<HTMLInputElement>, key: string) {
+    if (key === SELECT_ALL_KEY) {
+      selectAll(e.target.checked)
+    } else {
+      selectAll(false)
+    }
+  }
+
   return (
     <main className="h-screen w-screen bg-slate-800 p-10 flex justify-center items-center">
       <div className="flex flex-col">
-        {countries.map((item, index) => (
-          <label
-            key={`check-${item}`}
-            className={`cursor-pointer ${index > 0 ? 'mt-3' : ''}`}
-          >
+        <label key="check-all" className="cursor-pointer">
+          <input
+            type="checkbox"
+            id="check-all"
+            onChange={(event) => handleClick(event, 'all')}
+          />
+          <span className="ml-1">{'Select All'}</span>
+        </label>
+
+        {countries.map((item) => (
+          <label key={`check-${item}`} className="cursor-pointer mt-3">
             <input
               type="checkbox"
               id={`check-${item}`}
-              onChange={(event) => handleClick(event)}
+              onChange={(event) => handleClick(event, item)}
+              checked={isAllSelected ? true : undefined}
             />
             <span className="ml-1">{item}</span>
           </label>
